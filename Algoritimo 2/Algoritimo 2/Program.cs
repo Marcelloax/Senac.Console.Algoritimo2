5942143987ff7ced45be263c5b28d9a6b3d84a97
Marcelloax <marcellomachado2014@gmail.com>
2025-03-25 21:51:03 -0300

    Leitura de Variaveis

--- Algoritimo 2/Algoritimo 2/Program.cs
+++ Algoritimo 2/Algoritimo 2/Program.cs
@@ -4,3 +4,15 @@ decimal preco = 20M;
 char letra = 'a';
 var nomeCompleto = "Marcello Silveira Machado";
 var altura = 180;
+Console.WriteLine("Digite seu Nome");
+nomeCompleto = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Bem Vindo " + nomeCompleto);
+Console.Write("Digite sua Idade: ");
+var linha = Console.ReadLine();
+idade = int.Parse(linha);
+Console.WriteLine("Idade informada" + idade);
+if (idade < 18)
+    Console.WriteLine("Você não pode estar aqui!");
+
+Console.ReadKey();
